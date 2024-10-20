04c10931fdd76c4cdd1743a486a01c2a96d641f5
dominionism <cainglet@uw.edu>
2024-10-19 23:58:49 -0700

    Completed BigGoButton

---
+++ app/src/main/java/edu/uw/ischool/cainglet/biggobutton/MainActivity.kt
@@ -0,0 +1,34 @@
+package edu.uw.ischool.cainglet.biggobutton
+import android.annotation.SuppressLint
+import kotlin.random.Random
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private var counter: Int = 0
+    private lateinit var primaryButton: Button
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        primaryButton = findViewById(R.id.btn_primary)
+        primaryButton.setOnClickListener {
+            val randomTextColor = Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+            val randomBackgroundColor = Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+
+            primaryButton.setTextColor(randomTextColor)
+            primaryButton.setBackgroundColor(randomBackgroundColor)
+            counter++
+            if (counter == 1) {
+                primaryButton.text = "You have pushed me $counter time!"
+            } else if (counter > 1) {
+                primaryButton.text = "You have pushed me $counter times!"
+            }
+        }
+    }
+}
